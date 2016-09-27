1b500f26d0cd5a5ef805ffd6081b2fc0a446ea31
juliejinjia <1512969475@qq.com>
2016-09-27 21:46:22 +0800

    exercise2
    how to use commit

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# A commet, this is so you can read your program later.
+
+#Anything after the # is ignored by python.
+
+print "Icould have code like this." # and the comm  after is ignored
+
+#You can also use a commet to "disable" or comment out a piece of code :
+
+print "This won't run."
+
+print "This will run."
