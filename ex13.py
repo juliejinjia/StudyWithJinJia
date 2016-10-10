39b7d1f2112403edd7a8eee20d980b09d19dc62c
juliejinjia <1512969475@qq.com>
2016-10-10 22:25:24 +0800

    调用程序块

---
+++ ex13.py
@@ -0,0 +1,8 @@
+#-coding:utf8-
+from sys import argv
+script,frist,second,third = argv
+ 
+print "The script is called:",script
+print "Your first variable is:",frist
+print "Your second variable is:",second
+print "Your third variable is:",third
